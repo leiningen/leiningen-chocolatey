d305d0dca5918315f94987a16b34619014cf049b
Florian Anderiasch <fa@art-core.org>
2017-09-13 22:32:48 +0200

    Add checksum, bump dep to jdk8

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 This is the [Leiningen](https://leiningen.org) package for [chocolatey.org](https://chocolatey.org).
 
 Forked from https://github.com/Iristyle/ChocolateyPackages
+
+## Recommended reading for releases
+
+  * https://chocolatey.org/docs/create-packages#package-fix-version-notation
+  * https://chocolatey.org/docs/create-packages#build-your-package
--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,6 @@
 $package = 'Leiningen'
 $version = '2.7.1'
+$checksum = '32385e54b54ec99ac8a37792347ca4f1a3c7feb792066d7ffc8f1e4c5b7c7ad1'
 
 $url = "https://raw.githubusercontent.com/technomancy/leiningen/$version/bin/lein.bat"
 
@@ -10,7 +11,7 @@ Write-Host "Downloading from $url"
 
 # Download from an HTTPS location
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Get-ChocolateyWebFile -PackageName "$package" -FileFullPath "$lein" -Url "$url"
+Get-ChocolateyWebFile -PackageName "$package" -FileFullPath "$lein" -Url "$url" -CheckSumType "sha256" -Checksum "$checksum"
 Write-Host "Download from $url complete"
 
 Write-Host "Executing bootstrap script from $batDir"
